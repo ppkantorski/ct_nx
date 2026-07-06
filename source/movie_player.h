acc9ac9188750abcd3f21f7be0b01cdeeae57b26
ppkantorski <6467366+ppkantorski@users.noreply.github.com>
2026-07-05 20:58:04 -0700

    Add copyright notices to movie_player.h
    Added copyright information for authors NaGaa95 and ppkantorski.

--- source/movie_player.h
+++ source/movie_player.h
@@ -1,5 +1,8 @@
 /* movie_player.h -- FMV cutscene playback for the Chrono Trigger Switch port
  *
+ * Copyright (C) 2026 NaGaa95 <https://github.com/NaGaa95>
+ * Copyright (C) 2026 ppkantorski <https://github.com/ppkantorski>
+ *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
  */
